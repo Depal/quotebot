39e28264673e8f901b362b0bb4fad429d0705f61
Pekhota Vadim <pekhotavs@gmail.com>
2022-04-03 01:16:07 +0700

    Draws something

--- go.mod
+++ go.mod
@@ -3,11 +3,15 @@ module github.com/Depal/quotebot
 go 1.17
 
 require (
+	github.com/fogleman/gg v1.3.0
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/jackc/pgx v3.6.2+incompatible
 	github.com/jmoiron/sqlx v1.3.4
 	github.com/joho/godotenv v1.4.0
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/tucnak/telebot v2.0.0+incompatible
 	go.uber.org/zap v1.21.0
+	golang.org/x/image v0.0.0-20220321031419-a8550c1d254a
 )
 
 require (
@@ -20,5 +24,5 @@ require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
-	golang.org/x/text v0.3.3 // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
--- internal/bot/commandQuote.go
+++ internal/bot/commandQuote.go
@@ -2,13 +2,15 @@ package bot
 
 import (
 	"github.com/Depal/quotebot/internal/bot/static"
+	"github.com/fogleman/gg"
+	"github.com/golang/freetype/truetype"
+	"github.com/nfnt/resize"
 	"github.com/tucnak/telebot"
+	"golang.org/x/image/font/gofont/goregular"
 	"image"
-	"image/color"
-	"image/draw"
-	"image/png"
+	_ "image/jpeg"
+	"log"
 	"net/http"
-	"os"
 )
 
 func (s *Service) handleQuote(message *telebot.Message) {
@@ -53,54 +55,82 @@ func (s *Service) handleQuote(message *telebot.Message) {
 
 	s.log.Debug(userpicUrl)
 
-	res, err := http.Get("http://i.imgur.com/m1UIjW1.jpg")
+	res, err := http.Get(userpicUrl)
+	if err != nil {
+		s.log.Fatal(err)
+		return
+	}
+	defer res.Body.Close()
+
+	s.log.Debug(res.Body)
+
+	userpic, _, err := image.Decode(res.Body)
 	if err != nil {
 		s.log.Fatal(err)
 		return
 	}
 
-	s.log.Debug(res.Body)
+	resizedUserpic := resize.Resize(96, 96, userpic, resize.Lanczos3)
+
+	width := 500.0
+	height := 200.0
+
+	dc := gg.NewContext(int(width), int(height))
+	dc.SetRGBA(1, 1, 1, 0)
+	dc.Clear()
+	dc.SetRGBA(0, 0, 0, 0)
+
+	s.log.Debug("drawing avatar")
+	dc.DrawCircle(53, 53, 48)
+	dc.Clip()
+	dc.DrawImage(resizedUserpic, 5, 5)
+	dc.ResetClip()
+
+	s.log.Debug("drawing messagebox")
+	dc.SetRGB(50, 50, 75)
+	dc.DrawRoundedRectangle(110, 5, 380, 190, 32)
+	dc.Fill()
+
+	s.log.Debug("drawing username")
+	font, err := truetype.Parse(goregular.TTF)
+	if err != nil {
+		log.Fatal(err)
+	}
+	face := truetype.NewFace(font, &truetype.Options{Size: 24})
+	dc.SetFontFace(face)
+	dc.SetRGB(0, 0, 0)
+	dc.DrawStringAnchored(quoted.Sender.FirstName, 170, 30, 0.5, 0.5)
+
+	s.log.Debug("drawing message")
+	face = truetype.NewFace(font, &truetype.Options{Size: 20})
+	dc.SetFontFace(face)
+	dc.SetRGB(0, 0, 0)
+	dc.DrawStringWrapped(quoted.Text, 300, 80, 0.5, 0.5, 300, 1, gg.AlignLeft)
+
+	dc.SavePNG("test.png")
 
 	/*
-		data, err := ioutil.ReadAll(res.Body)
+
+		background := image.NewRGBA(image.Rect(0, 0, 320, 240))
+		blue := color.RGBA{25, 25, 50, 255}
+		draw.Draw(background, background.Bounds(), &image.Uniform{C: blue}, image.Point{}, draw.Src)
+		draw.Draw(background, background.Bounds(), resizedUserpic, image.Point{}, draw.Src)
+
+		addLabel(background, 50, 50, message.Sender.FirstName)
+
+		f, err := os.Create("test.png")
 		if err != nil {
 			s.log.Fatal(err)
 			return
 		}
-		//fmt.Println(data)
+		defer f.Close()
 
-		defer func() {
-			err = res.Body.Close()
-			if err != nil {
-				s.log.Fatal(err)
-				return
-			}
-		}()
-
-		/*
-		img, _, err := image.Decode(bytes.NewReader(data))
-		if err != nil || img == nil {
+		err = png.Encode(f, background)
+		if err != nil {
 			s.log.Fatal(err)
 			return
 		}
 	*/
-
-	background := image.NewRGBA(image.Rect(0, 0, 640, 480))
-	blue := color.RGBA{255, 122, 255, 255}
-	draw.Draw(background, background.Bounds(), &image.Uniform{blue}, image.Point{}, draw.Src)
-
-	f, err := os.Create("test.png")
-	if err != nil {
-		s.log.Fatal(err)
-		return
-	}
-	defer f.Close()
-
-	err = png.Encode(f, background)
-	if err != nil {
-		s.log.Fatal(err)
-		return
-	}
 
 	file := &telebot.Photo{File: telebot.FromDisk("test.png")}
 
@@ -113,3 +143,50 @@ func (s *Service) handleQuote(message *telebot.Message) {
 
 	s.finishCommand(static.CommandQuote)
 }
+
+/*
+func addLabel(img *image.RGBA, x, y int, label string) {
+	var (
+		dpi      = flag.Float64("dpi", 72, "screen resolution in Dots Per Inch")
+		fontfile = flag.String("fontfile", "../../testdata/luxisr.ttf", "filename of the ttf font")
+		//hinting  = flag.String("hinting", "none", "none | full")
+		size     = flag.Float64("size", 12, "font size in points")
+		//spacing  = flag.Float64("spacing", 1.5, "line spacing (e.g. 2 means double spaced)")
+		//wonb     = flag.Bool("whiteonblack", false, "white text on a black background")
+	)
+
+	// Read the font data.
+	fontBytes, err := ioutil.ReadFile(*fontfile)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	f, err := freetype.ParseFont(fontBytes)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Initialize the context.
+	fg, bg := image.Black, image.White
+	rgba := image.NewRGBA(image.Rect(0, 0, 640, 480))
+	draw.Draw(rgba, rgba.Bounds(), bg, image.Point{}, draw.Src)
+
+	c := freetype.NewContext()
+	c.SetDPI(*dpi)
+	c.SetFont(f)
+	c.SetFontSize(*size)
+	c.SetClip(rgba.Bounds())
+	c.SetDst(rgba)
+	c.SetSrc(fg)
+
+
+	c.SetDst(img)
+	sizeD := 12.0 // font size in pixels
+	pt := freetype.Pt(x, y+int(c.PointToFixed(sizeD)>>6))
+
+	if _, err := c.DrawString(label, pt); err != nil {
+		// handle error
+	}
+}
+*/
